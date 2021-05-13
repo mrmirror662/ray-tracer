9b7f73a71e51bb0a9cb37969f80391c160bf61bc
mrmirror662 <62153832+mrmirror662@users.noreply.github.com>
2021-05-13 19:55:48 +0530

    Add files via upload

---
+++ raytracer/source/includes/shape.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <vector>
+#include <string>
+struct triangle
+{
+
+    glm::vec3 v1, v2, v3;
+    glm::vec3 col;
+    glm::vec3 normal;
+    triangle(glm::vec3 v1, glm::vec3 v2, glm::vec3 v3) : v1(v1), v2(v2), v3(v3)
+    {
+        col = {0.1f, 0.1f, 0.1f};
+        normal = glm::cross(v2 - v1, v3 - v1);
+        normal = glm::normalize(normal);
+    };
+    triangle(glm::vec3 v1, glm::vec3 v2, glm::vec3 v3, glm::vec3 col) : v1(v1), v2(v2), v3(v3), col(col)
+    {
+        normal = glm::cross(v2 - v1, v3 - v1);
+        normal = glm::normalize(normal);
+    };
+};
+struct mesh
+{
+    std::vector<triangle> tris;
+    bool loadFromObj(std::string filePath);
+};
